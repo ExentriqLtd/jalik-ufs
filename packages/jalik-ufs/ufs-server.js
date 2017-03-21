2a214da7b96f59243e50e264b52661854ac064d4
VeselyT <tomas@vesely.in>
2017-03-22 00:34:06 +0100

    reverted file serving support in debug mode to allow development without nginx configuration

--- packages/jalik-ufs/ufs-server.js
+++ packages/jalik-ufs/ufs-server.js
@@ -185,9 +185,184 @@ if (Meteor.isServer) {
                 res.end();
             });
         }
-        else if (req.method == 'GET') {
-          res.writeHead(404);
-          res.end();
+        else if (req.method == 'GET' && Meteor.settings.private.isDebug) {
+
+            // Get store, file Id and file name
+            let regExp = new RegExp('^\/([^\/\?]+)\/([^\/\?]+)(?:\/([^\/\?]+))?$');
+            let match = regExp.exec(path);
+
+            // Avoid 504 Gateway timeout error
+            // if file is not handled by UploadFS.
+            if (match === null) {
+                next();
+                return;
+            }
+
+            // Get store
+            const storeName = match[1];
+            const store = UploadFS.getStore(storeName);
+
+            if (!store) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+
+            if (store.onRead !== null && store.onRead !== undefined && typeof store.onRead !== 'function') {
+                console.error(`ufs: Store.onRead is not a function in store "${storeName}"`);
+                res.writeHead(500);
+                res.end();
+                return;
+            }
+
+            // Remove file extension from file Id
+            let index = match[2].indexOf('.');
+            let fileId = index !== -1 ? match[2].substr(0, index) : match[2];
+
+            // Get file from database
+            const file = store.getCollection().findOne({_id: fileId});
+            if (!file) {
+                res.writeHead(404);
+                res.end();
+                return;
+            }
+
+            // Simulate read speed
+            if (UploadFS.config.simulateReadDelay) {
+                Meteor._sleepForMs(UploadFS.config.simulateReadDelay);
+            }
+
+            d.run(() => {
+                // Check if the file can be accessed
+                if (store.onRead.call(store, fileId, file, req, res) !== false) {
+                    let options = {};
+                    let status = 200;
+
+                    // Prepare response headers
+                    let headers = {
+                        'Content-Type': file.type,
+                        'Content-Length': file.size
+                    };
+
+                    // Add ETag header
+                    if (typeof file.etag === 'string') {
+                        headers['ETag'] = file.etag;
+                    }
+
+                    // Add Last-Modified header
+                    if (file.modifiedAt instanceof Date) {
+                        headers['Last-Modified'] = file.modifiedAt.toUTCString();
+                    }
+                    else if (file.uploadedAt instanceof Date) {
+                        headers['Last-Modified'] = file.uploadedAt.toUTCString();
+                    }
+
+                    // Parse request headers
+                    if (typeof req.headers === 'object') {
+
+                        // Compare ETag
+                        if (req.headers['if-none-match']) {
+                            if (file.etag === req.headers['if-none-match']) {
+                                res.writeHead(304); // Not Modified
+                                res.end();
+                                return;
+                            }
+                        }
+
+                        // Compare file modification date
+                        if (req.headers['if-modified-since']) {
+                            const modifiedSince = new Date(req.headers['if-modified-since']);
+
+                            if ((file.modifiedAt instanceof Date && file.modifiedAt > modifiedSince)
+                                || file.uploadedAt instanceof Date && file.uploadedAt > modifiedSince) {
+                                res.writeHead(304); // Not Modified
+                                res.end();
+                                return;
+                            }
+                        }
+
+                        // Send data in range
+                        if (typeof req.headers.range === 'string') {
+                            let range = req.headers.range;
+
+                            // Range is not valid
+                            if (!range) {
+                                res.writeHead(416);
+                                res.end();
+                                return;
+                            }
+
+                            let positions = range.replace(/bytes=/, '').split('-');
+                            let start = parseInt(positions[0], 10);
+                            let total = file.size;
+                            let end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+
+                            // Update headers
+                            headers['Content-Range'] = `bytes ${start}-${end}/${total}`;
+                            headers['Accept-Ranges'] = `bytes`;
+                            headers['Content-Length'] = (end - start) + 1;
+
+                            status = 206; // partial content
+                            options.start = start;
+                            options.end = end;
+                        }
+                    }
+
+                    // Open the file stream
+                    let rs = store.getReadStream(fileId, file, options);
+                    let ws = new stream.PassThrough();
+
+                    rs.on('error', Meteor.bindEnvironment((err) => {
+                        store.onReadError.call(store, err, fileId, file);
+                        res.end();
+                    }));
+                    ws.on('error', Meteor.bindEnvironment((err) => {
+                        store.onReadError.call(store, err, fileId, file);
+                        res.end();
+                    }));
+                    ws.on('close', () => {
+                        // Close output stream at the end
+                        ws.emit('end');
+                    });
+
+                    // Transform stream
+                    store.transformRead(rs, ws, fileId, file, req, headers);
+
+                    // Parse request headers
+                    if (typeof req.headers === 'object') {
+                        // Compress data using if needed (ignore audio/video as they are already compressed)
+                        if (typeof req.headers['accept-encoding'] === 'string' && !/^(audio|video)/.test(file.type)) {
+                            let accept = req.headers['accept-encoding'];
+
+                            // Compress with gzip
+                            if (accept.match(/\bgzip\b/)) {
+                                headers['Content-Encoding'] = 'gzip';
+                                delete headers['Content-Length'];
+                                res.writeHead(status, headers);
+                                ws.pipe(zlib.createGzip()).pipe(res);
+                                return;
+                            }
+                            // Compress with deflate
+                            else if (accept.match(/\bdeflate\b/)) {
+                                headers['Content-Encoding'] = 'deflate';
+                                delete headers['Content-Length'];
+                                res.writeHead(status, headers);
+                                ws.pipe(zlib.createDeflate()).pipe(res);
+                                return;
+                            }
+                        }
+                    }
+
+                    // Send raw data
+                    if (!headers['Content-Encoding']) {
+                        res.writeHead(status, headers);
+                        ws.pipe(res);
+                    }
+
+                } else {
+                    res.end();
+                }
+            });
         } else {
             next();
         }
